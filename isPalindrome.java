76496a268e29597e56863f75ebb5e59e4f26b340
Pernjek <pernjek.anto@gmail.com>
2021-01-19 22:32:19 +0100

    .

--- isPalindrome.java
+++ isPalindrome.java
@@ -7,20 +7,22 @@ class isPalindrome
       String input;
       Scanner sc = new Scanner(System.in);
         
-      System.out.println("Enter text here:");
+      System.out.println("Enter text here\nto check if palindrome:");
       input = sc.nextLine();
 
-      if (isPalindrome(input))
+      if (checkIfPalindrome(input))
       {
         System.out.println("\nIs palindrome.\n");
       }
       else {
           System.out.println("\nNot palindrome.\n");
       }
-      
+      if ( sc != null ) {
+          sc.close ();
+      }
       
     }
-    public static boolean isPalindrome (String input ) 
+    public static boolean checkIfPalindrome (String input ) 
     {
         input=input.replaceAll("[^a-zA-Z]", "").toLowerCase();  
         int length = input.length();
